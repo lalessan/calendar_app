2ca6f3bd6870ab551f4d76fb8d907a178be17b55
Laura Alessandretti <l.alessandretti@gmail.com>
2023-06-05 12:21:22 +0200

    changes

--- session-details.js
+++ session-details.js
@@ -15,10 +15,34 @@ function displaySessionDetails(session) {
   // Set session title
   sessionTitleElement.textContent = session.title;
 
-  // Create list items for presentations
+  // Clear any existing content in the presentation list
+  presentationListElement.innerHTML = '';
+
+  // Create presentation elements and append them to the presentation list
   session.presentations.forEach(presentation => {
-    let listItem = document.createElement('li');
-    listItem.textContent = `Title: ${presentation.title}, Speaker: ${presentation.speaker}`;
-    presentationListElement.appendChild(listItem);
+    let presentationItem = document.createElement('div');
+    presentationItem.classList.add('presentation-item');
+
+    let timeElement = document.createElement('div');
+    timeElement.classList.add('time');
+    timeElement.textContent = presentation.presentation_time;
+
+    let presentationInfo = document.createElement('div');
+    presentationInfo.classList.add('presentation-info');
+
+    let titleElement = document.createElement('div');
+    titleElement.classList.add('title');
+    titleElement.textContent = `${presentation.title}`;
+
+    let presenterElement = document.createElement('div');
+    presenterElement.classList.add('presenter');
+    presenterElement.textContent = `Presenter: ${presentation.presenter}`;
+
+    presentationInfo.appendChild(titleElement);
+    presentationInfo.appendChild(presenterElement);
+
+    presentationItem.appendChild(timeElement);
+    presentationItem.appendChild(presentationInfo);
+    presentationListElement.appendChild(presentationItem);
   });
 }
